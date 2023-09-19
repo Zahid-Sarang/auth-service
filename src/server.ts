f0e51b9bad96f6df7e5a2b0eececc18c4a4adae5
zahid-soft <zahidsarang95@gmail.com>
2023-09-19 18:18:13 +0530

    test lint staged 2

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 function login(username: string): string {
     const user = {
         name: "login",
+        name2: "login",
     };
     const password = user.name;
     return username + password;
