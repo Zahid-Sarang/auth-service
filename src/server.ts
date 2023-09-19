d4febddc10c9bd4c28cec756a35f8a92150a20b7
zahid-soft <zahidsarang95@gmail.com>
2023-09-19 17:49:32 +0530

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-function login(username: string): boolean {
-    return true;
+function login(username: string): string {
+    console.log(username);
+    return username;
 }
 
-login('zahid');
+login("zahid");
