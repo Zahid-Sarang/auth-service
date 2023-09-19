dd63d15772b1b4c708fca9bbaeb30302c6df72b3
zahid-soft <zahidsarang95@gmail.com>
2023-09-19 22:01:33 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
-    const PORT = Config.PORT;
-    try {
-        app.listen(PORT, () => {
-            // eslint-disable-next-line no-console
-            console.log(`Listing on port ${PORT}`);
-        });
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(`Failed to listen on port ${PORT}`, error);
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      logger.info(`Listing on port ${PORT}`);
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      logger.error(error.message);
+      setTimeout(() => {
         process.exit(1);
+      }, 1000);
     }
+  }
 };
 
 startServer();
