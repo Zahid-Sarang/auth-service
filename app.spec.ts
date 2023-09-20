74195c34d223ac0a3b9c87d2d0136cf14a9ddb44
zahid-soft <zahidsarang95@gmail.com>
2023-09-20 12:30:22 +0530

    add jest test case

---
+++ app.spec.ts
@@ -0,0 +1,14 @@
+import { calculateDiscount } from "./src/config/utlis";
+import request from "supertest";
+import app from "./src/app";
+describe("App", () => {
+  it("should calculate the discount", () => {
+    const result = calculateDiscount(100, 10);
+    expect(result).toBe(10);
+  });
+
+  it("should return 200 status", async () => {
+    const response = await request(app).get("/").send();
+    expect(response.statusCode).toBe(200);
+  });
+});
