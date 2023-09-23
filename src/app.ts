52fadd4e46902192149ba283bfe4ba59d3860a74
zahid-soft <zahidsarang95@gmail.com>
2023-09-23 10:21:51 +0530

    use TDD write and test case for register controller

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
 import logger from "./config/logger";
+import authRouter from "./routes/auth";
 
 const app = express();
 
 app.get("/", async (req, res) => {
   res.send("welcome to the app");
 });
+
+app.use("/auth", authRouter);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
---
+++ tests/users/register.spec.ts
@@ -0,0 +1,41 @@
+import request from "supertest";
+import app from "../../src/app";
+
+describe("POST /auth/register", () => {
+  describe("Given all fields", () => {
+    it("should return the 201 status code", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Smith",
+        email: "zahid@gmail.com",
+        password: "secret",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      expect(response.statusCode).toBe(201);
+    });
+
+    it("should return valid json format", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Smith",
+        email: "zahid@gmail.com",
+        password: "secret",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      expect(
+        (response.headers as Record<string, string>)["content-type"],
+      ).toEqual(expect.stringContaining("json"));
+    });
+  });
+  describe("Fields are missing", () => {});
+});
