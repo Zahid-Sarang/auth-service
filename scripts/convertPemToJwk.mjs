5ede7a74157c5cc4a7109aa00d52b7a246fe516b
zahid-soft <zahidsarang95@gmail.com>
2023-10-23 20:23:40 +0530

    format code

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -5,4 +5,3 @@ const privateKey = fs.readFileSync("./certs/privateKey.pem");
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const jwk = rsaPemToJwk(privateKey, { use: "sig" }, "public");
-
