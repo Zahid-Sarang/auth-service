ced9f0ce99bd836b9e57249d39ab847c73142322
zahid-soft <zahidsarang95@gmail.com>
2023-11-07 22:45:17 +0530

    add test for exclude passwords

--- tests/users/getUsers.spec.ts
+++ tests/users/getUsers.spec.ts
@@ -87,5 +87,50 @@ describe("GET /users/allUsers", () => {
                 expect(userInResponse.email).toBe(userInDatabase.email);
             }
         });
+
+        it("should not return passwords of the user", async () => {
+            const userData = {
+                firstName: "zahid",
+                lastName: "sarang",
+                email: "zahid@gmail.com",
+                password: "password",
+                tenantId: 1,
+            };
+
+            const adminToken = jwks.token({
+                sub: "1",
+                role: Roles.ADMIN,
+            });
+
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...userData,
+                role: Roles.ADMIN,
+            });
+
+            const users = await userRepository.find();
+
+            // Add token to cookie
+            const response = await request(app)
+                .get("/users/allUsers")
+                .set("Cookie", [`accessToken=${adminToken}`])
+                .send();
+
+            const keys = Object.keys(response.body as Record<string, string>);
+
+            const userArray = keys.map(
+                (key) => (response.body as Record<string, string>)[key],
+            );
+            expect(Array.isArray(userArray)).toBe(true);
+            expect(userArray.length).toBe(users.length);
+            for (let i = 0; i < users.length; i++) {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                const userInResponse = response.body[i] as Record<
+                    string,
+                    string
+                >;
+                expect(userInResponse).not.toHaveProperty("password");
+            }
+        });
     });
 });
