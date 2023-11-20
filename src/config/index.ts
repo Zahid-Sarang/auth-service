1c7dba67d19198ff7608a4aeb3066dc054a5cca9
zahid-soft <zahidsarang95@gmail.com>
2023-11-20 20:53:46 +0530

    set private key in env

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,6 +14,7 @@ const {
     DB_NAME,
     REFRESH_TOKEN_SECRET,
     JWKS_URI,
+    PRIVATE_KEY,
 } = process.env;
 
 export const Config = {
@@ -26,4 +27,5 @@ export const Config = {
     DB_NAME,
     REFRESH_TOKEN_SECRET,
     JWKS_URI,
+    PRIVATE_KEY,
 };
--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -1,5 +1,3 @@
-import fs from "fs";
-import path from "path";
 import { JwtPayload, sign } from "jsonwebtoken";
 import createHttpError from "http-errors";
 import { Config } from "../config";
@@ -10,11 +8,13 @@ import { Repository } from "typeorm";
 export class TokenService {
     constructor(private refreshTokenRepository: Repository<RefreshToken>) {}
     generateAccessToken(payload: JwtPayload) {
-        let privateKey: Buffer;
+        let privateKey: string;
+        if (!Config.PRIVATE_KEY) {
+            const error = createHttpError(500, "SECRET_KEY is not set");
+            throw error;
+        }
         try {
-            privateKey = fs.readFileSync(
-                path.join(__dirname, "../../certs/privateKey.pem"),
-            );
+            privateKey = Config.PRIVATE_KEY;
         } catch (err) {
             const error = createHttpError(
                 500,
