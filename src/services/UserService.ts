59b47be3befbf7eb20454a7610856f65654d241c
zahid-soft <zahidsarang95@gmail.com>
2024-01-22 20:36:15 +0530

    add left join for tenant

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -109,6 +109,7 @@ export class UserService {
             });
         }
         const result = await queryBuilder
+            .leftJoinAndSelect("user.tenant", "tenant")
             .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
             .take(validatedQuery.perPage)
             .orderBy("user.id", "DESC")
